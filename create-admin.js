06c0b39d4dfc53392b367f821a85c9d3b2400064
Hasnain Makada <hasnainmakada@gmail.com>
2025-01-18 17:07:06 +0530

    add: fourth Commit

--- create-admin.js
+++ create-admin.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./config.env" });
+// require("dotenv").config({ path: "./config.env" });
 const mongoose = require("mongoose");
 const Admin = require("./src/models/admin");
 
